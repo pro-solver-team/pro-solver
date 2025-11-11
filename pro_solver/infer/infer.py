f9f2f1f4863042f0ec438168ae8bdc7ec663ee92
aruna <shamil.a@phystech.edu>
2025-11-11 15:09:08 +0300

    Minor config changes

--- pro_solver/infer/infer.py
+++ pro_solver/infer/infer.py
@@ -1,29 +1,22 @@
 import hydra
 from omegaconf import DictConfig
-from pathlib import Path
-
+import fire
 from pro_solver.modules.rag_pipeline.base_model import LLMModel
 from pro_solver.modules.rag_pipeline.full_pipeline import RagPipeline
 from pro_solver.modules.collection.collection import load_collection
-from config.database.config import DB_DIR, COLLECTION_NAME
 
-root_path = Path().resolve().parents[1]
-config_rel_path = "config/infer"
-config_path = root_path/config_rel_path
-config_name = "config.yaml"
 
-@hydra.main(config_path=str(config_path), config_name=str(config_name))
+@hydra.main(version_base=None, config_path="../../config", config_name="infer/config")
 def main(cfg: DictConfig):
-    model = LLMModel(api_key = cfg.api_key, model_name = cfg.LLM_NAME)
-    collection = load_collection(DB_DIR, COLLECTION_NAME)
+    model = LLMModel(api_key = cfg.api_key, model_name = cfg.llm_name)
+    collection = load_collection(cfg.db_dir, cfg.collection_name)
 
     #----- RAG ------
     rag_dict = cfg.math_config
     code_dict = cfg.code_config
     pipeline = RagPipeline(model, rag_dict, code_dict, collection)
 
-    pipeline('shit.py')
+    pipeline(cfg.output_solver_name)
 
 if __name__ == "__main__":
-    main()
-    #fire.Fire(main)
+    fire.Fire()
---
+++ pro_solver/infer/vars/infer_vars/model_var.py
@@ -0,0 +1,3 @@
+llm_name = "mistral-small-latest"
+db_dir = "../../data/chroma"
+collection_name = "math"
