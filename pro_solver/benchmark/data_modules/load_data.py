64dd83270c254a7e7f3c3c6042bd47776c5f3ada
bahanmansh <yahasham.26@gmail.com>
2025-11-18 13:11:15 +0300

    add burgers equation

--- pro_solver/benchmark/data_modules/load_data.py
+++ pro_solver/benchmark/data_modules/load_data.py
@@ -1,5 +1,5 @@
 import h5py
-from pro_solver.benchmark.data_modules.data_vars import darcy2d_path, reacdiff1d_path
+from pro_solver.benchmark.data_modules.data_vars import darcy2d_path, reacdiff1d_path, burgers1d_path
 
 def equation_to_numpy(equation_name: str):
     if equation_name == 'darcy2d':
@@ -19,6 +19,16 @@ def equation_to_numpy(equation_name: str):
             target = f['tensor'][:]
             u_0 = target[:, 0]
         return t, x, u_0, target
+
+    elif equation_name == 'burgers1d':
+        file_path = burgers1d_path
+        with h5py.File(file_path, 'r') as f:
+            t = f['t-coordinate'][:-1]
+            x = f['x-coordinate'][:]
+            target = f['tensor'][:]
+            u_0 = target[:, 0]
+        return t, x, u_0, target
+
     else:
         raise ValueError("no equation dodik")
 
